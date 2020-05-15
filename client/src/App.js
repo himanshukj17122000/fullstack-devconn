6cc5592535ae0f5a3dc7c5227cbc563d4e6dcca1
Himanshu Jain <hkj4@dj4.edu>
2020-05-15 16:49:55 +0530

    Made the post component

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import { loadUser } from './actions/auth';
 //Redux
 import { Provider } from 'react-redux';
@@ -64,6 +65,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />{' '}
+              <PrivateRoutes exact path='/posts' component={Posts} />{' '}
             </Switch>{' '}
           </section>{' '}
         </Fragment>{' '}
