789614e262a2fbdfdf65d9802540d2827da399a0
Himanshu Jain <hkj4@dj4.edu>
2020-05-16 16:32:51 +0530

    production ready

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express')
 const app = express()
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000;
+const path = require('path')
 
 
 //connect Database
 connectDB()
-app.get('/', (req, res) => {
-    res.send('API running')
-})
+
 
 //Init Middleware 
 app.use(express.json({
@@ -19,6 +18,17 @@ app.use('/api/users', require('./config/routes/api/users'))
 app.use('/api/profile', require('./config/routes/api/profile'))
 app.use('/api/posts', require('./config/routes/api/posts'))
 app.use('/api/auth', require('./config/routes/api/auth'))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 app.listen(PORT, () => {
     console.log(`Listening at the port ${PORT}`)
